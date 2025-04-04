6e37b59223bdba01aab418093c29bdc973413e0b
k4084 <k40840096@gmail.com>
2025-04-04 16:50:48 +0800

    rr

--- countPath/server.py
+++ countPath/server.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify
+from threading import Thread
 from flask_cors import CORS
 from findRoute import findRoute
 from read_GIPS_distance import UWBpos
-
 app = Flask(__name__)
 CORS(app)
 pos = UWBpos()
-
-
-print("🚀 正在執行這份 server.py（版本 A）")
+# pos.recalibrate()
 
 
 @app.route('/dest', methods=['POST'])
@@ -22,26 +20,18 @@ def dest():
     }
     return jsonify(response_data), 200
 
+
 @app.route('/pos')
 def getPos():
-    print("🛰️ /pos 被呼叫了 (開始)")
-
-    try:
-        print("➡️ 呼叫 pos.fake_read()")
-        pos.fake_read()
-
-        print("➡️ 呼叫 pos.compute_CRS()")
-        x, y = pos.compute_CRS()
-
-        print(f"🎯 回傳成功座標: ({y}, {x})")
-        return jsonify([x, y]), 200
-
-    except Exception as e:
-        print("❌ /pos 發生錯誤：", e)
-        return jsonify({"error": str(e)}), 500
-
-
-
+    print("call getPos")
+    # pos.recalibrate()
+    # pos.fake_read()     # if you don't have UWB module, use this
+    pos.UWB_read()      # if you have UWB module, use this
+    # x, y = pos.compute_CRS()
+    x, y = pos.UWB_read_compute_CRS_5()
+    # x, y = pos.get_anchor_CRS('9')
+    print(f"coordinate: {y}, {x}")
+    return jsonify([x, y]), 200
 
 
 @app.route('/pos/anchor/<anchor_number>')
@@ -49,10 +39,57 @@ def getAnchorPos(anchor_number):
     x, y = pos.get_anchor_CRS(anchor_number)
     return jsonify([x, y]), 200
 
+
 @app.route('/pos/recalibrate')
 def recalibrate():
     x, y = pos.recalibrate()
     return jsonify([x, y]), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500, debug=True)
+
+app = Flask(__name__)
+CORS(app)
+pos = UWBpos()
+# pos.recalibrate()
+
+
+@app.route('/dest', methods=['POST'])
+def dest():
+    print('Destination received')
+    print(request.json['dest'])
+
+    route = findRoute(request.json['st'], request.json['dest'])
+    response_data = {
+        'route': route
+    }
+    return jsonify(response_data), 200
+
+
+@app.route('/pos')
+def getPos():
+    # pos.recalibrate()
+    # pos.fake_read()     # if you don't have UWB module, use this
+    pos.UWB_read()      # if you have UWB module, use this
+    # x, y = pos.compute_CRS()
+    x, y = pos.UWB_read_compute_CRS_5()
+    # x, y = pos.get_anchor_CRS('6')
+    print(f"coordinate: {y}, {x}")
+    return jsonify([x, y]), 200
+
+
+@app.route('/pos/anchor/<anchor_number>')
+def getAnchorPos(anchor_number):
+    x, y = pos.get_anchor_CRS(anchor_number)
+    return jsonify([x, y]), 200
+
+
+@app.route('/pos/recalibrate')
+def recalibrate():
+    x, y = pos.recalibrate()
+    return jsonify([x, y]), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500, debug=True)
